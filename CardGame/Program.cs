0044b395b523789af6650a02ea6b223c7468f325
nonex97 <ngrguric97@gmail.com>
2026-07-23 21:09:39 +0200

    Built a deck for poker and written the cards to the console.

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -12,6 +12,12 @@ namespace CardGame
         {
             PokerDeck deck = new PokerDeck();
 
+            var hand = deck.DealCards();
+
+            foreach (var card in hand)
+            {
+                Console.WriteLine($"{card.Value.ToString()} of {card.Suit.ToString()}");
+            }
 
             Console.ReadLine();
         }
