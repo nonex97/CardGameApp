7bb36bcb26f27f9b0ae40f4536f075ef36ea0c96
nonex97 <ngrguric97@gmail.com>
2026-07-23 20:54:46 +0200

    Implemented methods to create a deck (populate, shuffle), to draw one card, and to create a poker deck.

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -17,23 +17,58 @@ namespace CardGame
 
     public abstract class Deck
     {
+        protected List<PlayingCard> fullDeck = new List<PlayingCard>();
+        protected List<PlayingCard> drawPile = new List<PlayingCard>();
+        protected List<PlayingCard> discardPile = new List<PlayingCard>();
+
         public void CreateDeck()
         {
-            
+            fullDeck.Clear();
+
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int val = 0; val < 13; val++)
+                {
+                    fullDeck.Add(new PlayingCard { Suit = (CardSuit)suit, Value = (CardValue)val });
+                }
+            }
         }
 
         public virtual void ShuffleDeck()
         {
+            var rnd = new Random();
+            drawPile = fullDeck.OrderBy(x => rnd.Next()).ToList();
+        }        
 
-        }
-
-        public abstract List<PlayingCard> DealCard(); // we don't know how to deal cards, but we know every game needs to deal cards
+        public abstract List<PlayingCard> DealCards(); // we don't know how to deal cards, but we know every game needs to deal cards
 
         public virtual PlayingCard RequestCard()
         {
-
+            PlayingCard output = drawPile.Take(1).First(); // take one fromthe draw pile
+            drawPile.Remove(output); // remove the same card from the draw pile
+            return output;
         }
 
+    }
+
+    public class PokerDeck : Deck
+    {
+        public PokerDeck()
+        {
+            CreateDeck();
+            ShuffleDeck();
+        }
+        public override List<PlayingCard> DealCards()
+        {
+            List<PlayingCard> output = new List<PlayingCard>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                output.Add(RequestCard());
+            }
+
+            return output;
+        }
     }
 
     public class PlayingCard
