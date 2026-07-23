fcc5410eafc7f21b0a0caa901ac18d4937d4811c
nonex97 <ngrguric97@gmail.com>
2026-07-23 20:26:37 +0200

    Created a structure of classes for card games..

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -14,4 +14,56 @@ namespace CardGame
             Console.ReadLine();
         }
     }
+
+    public abstract class Deck
+    {
+        public void CreateDeck()
+        {
+            
+        }
+
+        public virtual void ShuffleDeck()
+        {
+
+        }
+
+        public abstract List<PlayingCard> DealCard(); // we don't know how to deal cards, but we know every game needs to deal cards
+
+        public virtual PlayingCard RequestCard()
+        {
+
+        }
+
+    }
+
+    public class PlayingCard
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
 }
